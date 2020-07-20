a973fc873469b5cb65c942da4125770c626ef878
alexsteed <alexsteed2020@gmail.com>
2020-07-20 22:04:24 +0300

    Версия 1

---
+++ src/main/java/ru/otus/akutsev/books/dao/BookDao.java
@@ -0,0 +1,24 @@
+package ru.otus.akutsev.books.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.otus.akutsev.books.model.Author;
+import ru.otus.akutsev.books.model.Book;
+import ru.otus.akutsev.books.model.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookDao extends JpaRepository<Book, Long> {
+	Book save(Book book);
+
+	Optional<Book> findAById(long id);
+
+	List<Book> findAll();
+
+	void delete(Book book);
+}
